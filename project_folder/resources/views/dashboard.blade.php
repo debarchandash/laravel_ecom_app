e69e1642a6cee6c32b1089387cd107c5184f6a0e
debarchan.dash@nettantra.net <debarchan.dash@nettantra.net>
2023-04-11 13:06:32 +0530

    layout created

---
+++ project_folder/resources/views/dashboard.blade.php
@@ -0,0 +1,8 @@
+<x-layout.userlayout>
+    <x-slot name="title">
+        Dashboard
+    </x-slot>
+    <div class="grid justify-center">
+        <h1 class="bg-yellow-400 text-red-600 text-xl font-sans">Welcome To Dashboard Debarchan</h1>
+    </div>
+</x-layout.userlayout>
